2ae757344f9e792a48b2c568965923ce3f0c7229
Daniel <fishfry62@gmail.com>
2016-05-03 18:52:47 -0400

    Created separate VM for form

--- public/js/index.js
+++ public/js/index.js
@@ -46,37 +46,9 @@ class App {
 		this.$orderFormModal = $("#order-form-modal");
 		this.$orderForm = $("#order-form");
 
-		this.TIRE_SHINE_COST = 25;
-		this.INTERIOR_COST = 75;
-		this.WAX_COST = 50
-
 		this.isGuest = ko.observable(false);
-		this.showBillingAddress = ko.observable(false);
-		this.addShine = ko.observable(false);
-		this.addWax = ko.observable(false);
-		this.addInterior = ko.observable(false);
-		this.carSizeMultiples = [
-			new CarSizeMultiple("Small (2-4 door)", 1.0),
-			new CarSizeMultiple("SUV (5-door)", 1.2),
-			new CarSizeMultiple("XXL", 1.5)
-		];
-		this.selectedCarSize = ko.observable(this.carSizeMultiples[0]);
-		this.orderTotal = ko.computed(function(){
-			var total = parseFloat((32 + (self.addShine() ? self.TIRE_SHINE_COST : 0) + 
-				(self.addWax() ? self.WAX_COST : 0) + 
-				(self.addInterior() ? self.INTERIOR_COST : 0)) *
-				(self.selectedCarSize().multiple));
-			return total >= 100 ? total.toPrecision(5) : total.toPrecision(4);
-		});
 
-		this.orderSummary = ko.computed(function(){
-			return $.validator.format("Exterior Hand Wash{0}{1}{2}. {3} = {4}x cost multiple.", 
-				(self.addShine() ? ", Deep Tire Clean & Shine" : ""),
-				(self.addWax() ? ", Hand Wax & Buff" : ""),
-				(self.addInterior() ? ", Full Interior Cleaning" : ""),
-				self.selectedCarSize().size,
-				self.selectedCarSize().multiple.toString());
-		});
+		this.OrderFormVm = new OrderFormViewModel();
 
 		this._initValidation();
 	}
@@ -100,10 +72,10 @@ class App {
 			this.$orderFormModal.modal();
 			this.webSvc.GetAllAppointments()
 				.then(function(data){
-					console.log(data);
+					console.info(data);
 				})
 				.fail(function(err){
-					console.log(err);
+					console.error(err);
 				});
 		} else {
 			this.$loginModal.modal();
@@ -125,10 +97,14 @@ class App {
 	OnSubmit(){
 		if(this.$orderForm.valid())
 		{
-			// additionanl validation
-			// weather
-			// is date/time overbooked
-			// etc...
+			if(this.isGuest())
+			{
+				// Create tmp user
+			}
+			else
+			{
+				// Update existing user
+			}
 		}
 	};
 
@@ -176,10 +152,89 @@ class App {
 	};
 }
 
+class OrderFormViewModel {
+	constructor(){
+		var self = this;
+		
+		this.TIRE_SHINE_COST = 25;
+		this.INTERIOR_COST = 75;
+		this.WAX_COST = 50
+
+		this.addShine = ko.observable(false);
+		this.addWax = ko.observable(false);
+		this.addInterior = ko.observable(false);
+		this.showBillingAddress = ko.observable(false);
+
+		this.carSizeMultiples = [
+			new CarSizeMultiple("Small (2-4 door)", 1.0),
+			new CarSizeMultiple("SUV (5-door)", 1.2),
+			new CarSizeMultiple("XXL", 1.5)
+		];
+		this.selectedCarSize = ko.observable(this.carSizeMultiples[0]);
+
+		this.carConditions = ["Light Dirt", "Medium Dirt", "Heavy Dirt", "Filthy"];
+		this.condition = ko.observable(this.carConditions[0]);
+
+		this.timeRanges = ["9:00 - 12:00 PM", "12:00 - 3:00 PM", "3:00 - 6:00 PM", "6:00 - 9:00 PM"];
+		this.selectedTimeRange = ko.observable(this.timeRanges[0]);
+
+		this.date = ko.observable(moment().format("MM/DD/YY"));
+		this.make = ko.observable("");
+		this.model = ko.observable("");
+		this.color = ko.observable("");
+		this.tag = ko.observable("");
+
+		this.email = ko.observable("");
+		this.phone = ko.observable("");
+
+		this.street = ko.observable("");
+		this.city = ko.observable("");
+		this.state = ko.observable("");
+		this.zip = ko.observable("");
+		
+		this.billStreet = ko.observable("");
+		this.billCity = ko.observable("");
+		this.billState = ko.observable("");
+		this.billZip = ko.observable("");
+
+		this.ccName = ko.observable("");
+		this.ccNumber = ko.observable("");
+		
+		this.ccExpiryMos = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+		this.ccExpiryMo = ko.observable(this.ccExpiryMos[0]);
+
+		this.ccExpiryYrs = [];
+		var year = new Date().getFullYear();
+		for (var i = 0; i < 12; i++) {
+		    this.ccExpiryYrs.push((i + year).toString());
+		}
+		this.ccExpiryYr = ko.observable(this.ccExpiryYrs[0]);
+
+		this.cvv = ko.observable("");
+
+		this.orderTotal = ko.computed(function(){
+			var total = parseFloat((32 + (self.addShine() ? self.TIRE_SHINE_COST : 0) + 
+				(self.addWax() ? self.WAX_COST : 0) + 
+				(self.addInterior() ? self.INTERIOR_COST : 0)) *
+				(self.selectedCarSize().multiple));
+			return total >= 100 ? total.toPrecision(5) : total.toPrecision(4);
+		});
+
+		this.orderSummary = ko.computed(function(){
+			return $.validator.format("Exterior Hand Wash<br>{0}{1}{2}{3} = {4}x cost multiple.", 
+				(self.addShine() ? "Deep Tire Clean & Shine<br>" : ""),
+				(self.addWax() ? "Hand Wax & Buff<br>" : ""),
+				(self.addInterior() ? "Full Interior Cleaning<br>" : ""),
+				self.selectedCarSize().size,
+				self.selectedCarSize().multiple.toString());
+		});
+	}
+}
+
 class LocalStorageHelper{
 	constructor(storageType){
 		if(typeof(Storage) === "undefined") {
-			console.log("No local storage available.");
+			console.info("No local storage available.");
 		} else {
 			this.storageType = storageType;
 		}
@@ -210,34 +265,42 @@ class CarSizeMultiple {
 
 class WebService {
 	constructor(){
-		var self = this;
 		this.baseUrl = document.location.origin;
-		this.ajaxOptions = {
-			type: 'GET',
-			dataType: 'JSON',
-			error: self._onError.bind(self),
-			success: self._onSuccess.bind(self),
-			timeout: 10000
-		}
 		this.deferred = null;
 	}
 
 	GetAllAppointments(){
+		return this._executeAjaxCall('GET', "/api/getAllAppointments");
+	}
+
+	CreateUser(user){
+		return this._executeAjaxCall('PUT', "/api/createUser", user);
+	}
+
+	// 'data' is an optional param
+	_executeAjaxCall(type, ext, data){
 		this.deferred = $.Deferred();
-		this.ajaxOptions.url = this.baseUrl + "/api/getAllAppointments";
-		$.ajax(this.ajaxOptions);
+		$.ajax({
+			url: this.baseUrl + ext,
+			type: type,
+			dataType: 'JSON',
+			error: this._onError.bind(this),
+			success: this._onSuccess.bind(this),
+			timeout: 10000,
+			data: data ? JSON.stringify(data) : undefined
+		});
 		return this.deferred.promise();
 	}
 
 	_onSuccess(data, textStatus, jqXHR){
-		console.log(data, textStatus, jqXHR);
+		console.info(data, textStatus, jqXHR);
 		if(this.deferred){
 			this.deferred.resolve(data);
 		}
 	}
 
 	_onError(jqXHR, textStatus, err){
-		console.log(jqXHR, textStatus, err);
+		console.error(jqXHR, textStatus, err);
 		if(this.deferred){
 			this.deferred.reject(err);
 		}
