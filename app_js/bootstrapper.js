39ec3f0e0dc572399418826a13b452ff3844cbea
Daniel <fishfry62@gmail.com>
2016-06-24 00:39:22 -0400

    ...

--- app_js/bootstrapper.js
+++ app_js/bootstrapper.js
@@ -36,32 +36,32 @@ class Bootstrapper{
 	    });
 
 		// iOS fix for modal bug with virtual keyboard
-		if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
-		    $('.modal').on('show.bs.modal', function() {
-		        // Position modal absolute and bump it down to the scrollPosition
-		        $(this)
-		            .css({
-		                position: 'absolute',
-		                marginTop: $(window).scrollTop() + 'px',
-		                bottom: 'auto'
-		            });
-		        // Position backdrop absolute and make it span the entire page
-		        // after Boostrap positions it but before transitions finish
-		        setTimeout(()=> {
-		            $('.modal-backdrop').css({
-		                position: 'absolute', 
-		                top: 0, 
-		                left: 0,
-		                width: '100%',
-		                height: Math.max(
-		                    document.body.scrollHeight, document.documentElement.scrollHeight,
-		                    document.body.offsetHeight, document.documentElement.offsetHeight,
-		                    document.body.clientHeight, document.documentElement.clientHeight
-		                ) + 'px'
-		            });
-		        }, 0);
-		    });
-		}
+		// if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
+		//     $('.modal').on('show.bs.modal', function() {
+		//         // Position modal absolute and bump it down to the scrollPosition
+		//         $(this)
+		//             .css({
+		//                 position: 'absolute',
+		//                 marginTop: $(window).scrollTop() + 'px',
+		//                 bottom: 'auto'
+		//             });
+		//         // Position backdrop absolute and make it span the entire page
+		//         // after Boostrap positions it but before transitions finish
+		//         setTimeout(()=> {
+		//             $('.modal-backdrop').css({
+		//                 position: 'absolute', 
+		//                 top: 0, 
+		//                 left: 0,
+		//                 width: '100%',
+		//                 height: Math.max(
+		//                     document.body.scrollHeight, document.documentElement.scrollHeight,
+		//                     document.body.offsetHeight, document.documentElement.offsetHeight,
+		//                     document.body.clientHeight, document.documentElement.clientHeight
+		//                 ) + 'px'
+		//             });
+		//         }, 0);
+		//     });
+		// }
 
 		// Load Templates
 		const failureMsg = "Application failed to initialize.";
