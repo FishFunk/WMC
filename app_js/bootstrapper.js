ff6ff342a30a08d3e0f1b1edc0cec3c8ea95cd5f
Daniel <fishfry62@gmail.com>
2016-06-23 23:32:46 -0400

    attempt to fix scrolling

--- app_js/bootstrapper.js
+++ app_js/bootstrapper.js
@@ -26,6 +26,8 @@ class Bootstrapper{
 	        // Dividing by two centers the modal exactly, but dividing by three 
 	        // or four works better for larger screens.
 	        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+
+	        $('#body').css("height", dialog.height());
 	    }
 	    
 	    // Reposition when a modal is shown
--- public/script-compiled.js
+++ public/script-compiled.js
@@ -38,6 +38,8 @@ var Bootstrapper = function () {
 				// Dividing by two centers the modal exactly, but dividing by three
 				// or four works better for larger screens.
 				dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+
+				$('#body').css("height", dialog.height());
 			}
 
 			// Reposition when a modal is shown
