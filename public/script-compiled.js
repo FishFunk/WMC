0a9c5ff6e18900957871e9f46bfd6a9fa54f4b09
Daniel <fishfry62@gmail.com>
2016-06-24 00:54:45 -0400

    ...

--- public/script-compiled.js
+++ public/script-compiled.js
@@ -47,34 +47,6 @@ var Bootstrapper = function () {
 				$('.modal:visible').each(reposition);
 			});
 
-			// iOS fix for modal bug with virtual keyboard
-			// if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
-			//     $('.modal').on('show.bs.modal', function() {
-			//         // Position modal absolute and bump it down to the scrollPosition
-			//         $(this)
-			//             .css({
-			//                 position: 'absolute',
-			//                 marginTop: $(window).scrollTop() + 'px',
-			//                 bottom: 'auto'
-			//             });
-			//         // Position backdrop absolute and make it span the entire page
-			//         // after Boostrap positions it but before transitions finish
-			//         setTimeout(()=> {
-			//             $('.modal-backdrop').css({
-			//                 position: 'absolute',
-			//                 top: 0,
-			//                 left: 0,
-			//                 width: '100%',
-			//                 height: Math.max(
-			//                     document.body.scrollHeight, document.documentElement.scrollHeight,
-			//                     document.body.offsetHeight, document.documentElement.offsetHeight,
-			//                     document.body.clientHeight, document.documentElement.clientHeight
-			//                 ) + 'px'
-			//             });
-			//         }, 0);
-			//     });
-			// }
-
 			// Load Templates
 			var failureMsg = "Application failed to initialize.";
 			async.series([function (callback) {
