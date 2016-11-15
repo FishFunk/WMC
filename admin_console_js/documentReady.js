61d82dfc31d356bd106dbf3db5dd4003557b1d1a
Daniel <fishfry62@gmail.com>
2016-11-14 21:29:31 -0500

    fixed UI bug with admin console

--- admin_console_js/documentReady.js
+++ admin_console_js/documentReady.js
@@ -6,23 +6,6 @@ window.jQuery(document).ready(($)=>{
 	spinner = new LoadingSpinner();
 	webSvc = new WebService();
 
-	function reposition() {
-        var modal = $(this),
-            dialog = modal.find('.modal-dialog');
-        modal.css('display', 'block');
-        
-        // Dividing by two centers the modal exactly, but dividing by three 
-        // or four works better for larger screens.
-        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
-    }
-    
-    // Reposition when a modal is shown
-    $('.modal').on('show.bs.modal', reposition);
-    // Reposition when the window is resized
-    $(window).on('resize', function(){
-        $('.modal:visible').each(reposition);
-    });
-
 	webSvc.GetSystemSettings()
 		.then(settings =>{
 			Configuration = new Configuration(settings);
